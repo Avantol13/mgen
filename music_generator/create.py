6a045d9417a6c5c41860530aab6ba60434dd1ddb
Alex <avantol13@gmail.com>
2016-05-26 20:57:02 -0400

    Better handling of export methods
    Check for path, make it if it's not there, handle None

--- music_generator/create.py
+++ music_generator/create.py
@@ -27,13 +27,15 @@ from datetime import datetime
 import os
 import warnings
 import traceback
+import pickle
 
 class MusicGenerator(object):
     '''
     classdocs
     '''
 
-    def __init__(self, style, composition_title='Untitled', author_name='By: Al Gogh Rhythm'):
+    def __init__(self, style, composition_title='Untitled',
+                 author_name='By: Al Gogh Rhythm'):
         '''
         Constructor
         '''
@@ -46,9 +48,11 @@ class MusicGenerator(object):
         self.__time_signature = choice.choose_time_signature(self.style)
         self.__key = choice.choose_key(self.style.probabilities['keys'])
 
-    def add_melody_track(self, num_bars, location_to_add=1, style=None, times_to_repeat=0, octave_adjust=0):
+    def add_melody_track(self, num_bars, location_to_add=1, style=None,
+                         times_to_repeat=0, octave_adjust=0):
         '''
-        Adds a mingus Track containing bars of randomly generated melodies to the composition.
+        Adds a mingus Track containing bars of randomly generated melodies to
+        the composition.
         '''
 
         if style is None:
@@ -67,7 +71,7 @@ class MusicGenerator(object):
             key = key.upper()
             scale = choice.choose_scale(key, style.probabilities['minor_scales'])
 
-        for index in range(0, num_bars):
+        for _ in range(0, num_bars):
             # Create time for melody
             melody_timing = self._create_melody_timing(style.probabilities['timings'])
 
@@ -78,7 +82,8 @@ class MusicGenerator(object):
             chosen_notes = choice.choose_notes(number_notes, scale)
 
             # Combine melody time and notes into a mingus Bar object
-            bar_to_add = convert.convert_notes_to_bar(self.__key, melody_timing, chosen_notes,
+            bar_to_add = convert.convert_notes_to_bar(self.__key, melody_timing,
+                                                      chosen_notes,
                                                       self.__time_signature)
 
             #print('TESTING STUFF...')
@@ -91,15 +96,17 @@ class MusicGenerator(object):
         # Repeat chords per argument
         melody_track.bars += melody_track.bars * times_to_repeat
 
-        # Add empty bars to the front of the track to place melody at the location specified
-        # Note: Start at Bar #1
+        # Add empty bars to the front of the track to place melody at the
+        # location specified. Note: Start at Bar #1
         empty_bars_to_add = location_to_add - 1
-        melody_track = time.prepend_empty_bars_to_track(melody_track, empty_bars_to_add)
+        melody_track = time.prepend_empty_bars_to_track(melody_track,
+                                                        empty_bars_to_add)
 
         self.composition.add_track(melody_track)
 
-    def add_chords_track(self, num_bars=None, location_to_add=0, style=None, melody_track=None, 
-                         times_to_repeat=0, octave_adjust=0, force_mode_scale=False):
+    def add_chords_track(self, num_bars=None, location_to_add=0, style=None,
+                         melody_track=None, times_to_repeat=0, octave_adjust=0,
+                         force_mode_scale=False):
         '''
         Adds a track to the composition filled with chords
         TODO: Create chord length other than all whole notes
@@ -146,17 +153,20 @@ class MusicGenerator(object):
                                                                   repeat_times_to_fill)
 
             else:
-                raise AttributeError('Cannot find a chord progression to meet the requirement' +
-                                     ' for ' + str(num_bars) + ' bars.')
+                raise AttributeError('Cannot find a chord progression to' +
+                                     ' meet the requirement' + ' for ' +
+                                     str(num_bars) + ' bars.')
 
         # Number of bars was not specified, just pick a single chord progression
         else:
             raw_chord_progression = choice.choose_chord_progression(progression_probs)
 
-        chord_progression_notes = progressions.to_chords(raw_chord_progression, self.__key)
+        chord_progression_notes = progressions.to_chords(raw_chord_progression,
+                                                         self.__key)
 
         # Adjust octave
-        chord_progression = convert.change_octave(chord_progression_notes, octave_adjust)
+        chord_progression = convert.change_octave(chord_progression_notes,
+                                                  octave_adjust)
 
         # Convert it to a mingus track
         chord_track = convert.convert_chord_progression_to_track(self.__key, chord_progression,
@@ -166,10 +176,11 @@ class MusicGenerator(object):
         # Repeat chords per argument
         chord_track.bars += chord_track.bars * times_to_repeat
 
-        # Add empty bars to the front of the track to place chords at the location specified
-        # Note: Start at Bar #1
+        # Add empty bars to the front of the track to place chords at the
+        # location specified. Note: Start at Bar #1
         empty_bars_to_add = location_to_add - 1
-        chord_track = time.prepend_empty_bars_to_track(chord_track, empty_bars_to_add)
+        chord_track = time.prepend_empty_bars_to_track(chord_track,
+                                                       empty_bars_to_add)
 
         self.composition.add_track(chord_track)
 
@@ -182,7 +193,8 @@ class MusicGenerator(object):
             index = len(self.composition.tracks) - 1
 
         if index >= len(self.composition.tracks):
-            raise IndexError('Provided index for track in composition is out of bounds.')
+            raise IndexError('Provided index for track in composition is out' +
+                             ' of bounds.')
 
         self.composition.tracks.pop(index)
 
@@ -193,13 +205,14 @@ class MusicGenerator(object):
         if meter.is_valid(time_signature):
             self.__time_signature = time_signature
         else:
-            warnings.warn(time_signature + ' is not a valid time signature.', UserWarning)
+            warnings.warn(time_signature + ' is not a valid time signature.',
+                          UserWarning)
             traceback.print_stack()
 
     def set_key(self, key):
         '''
-        Set the key for the composition. Will randomly choose key by using the probabilities
-        in configuration file if one is not provided.
+        Set the key for the composition. Will randomly choose key by using the
+        probabilities in configuration file if one is not provided
         '''
         if keys.is_valid_key(key):
             self.__key = key
@@ -207,11 +220,20 @@ class MusicGenerator(object):
             warnings.warn(key + ' is not a valid key.', UserWarning)
             traceback.print_stack()
 
-    def export_pdf(self, file_path=None):
+    def export_pdf(self, file_path):
         '''
         Outputs a pdf to a specified path
         '''
         if file_path is None:
+            warnings.warn('PDF not generated. Please specify valid path.',
+                          UserWarning)
+            traceback.print_stack()
+            return
+
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+
+        if file_path.endswith('/'):
             # Create filename based on key and time
             file_path = (str(os.getcwd()) + '\output\\' +
                          str(datetime.now()).replace(' ', '_').replace(':', '.'))
@@ -221,36 +243,77 @@ class MusicGenerator(object):
         if ly_string and self.composition.tracks:
             LilyPond.to_pdf(ly_string, file_path)
         else:
-            warnings.warn('PDF not generated because the composition didn\'t have any tracks. :(',
-                          UserWarning)
+            warnings.warn('PDF not generated because the composition didn\'t' +
+                          'have any tracks. :(', UserWarning)
             traceback.print_stack()
 
-    def export_midi(self, file_name=None, bpm=100, repeat=0, verbose=False):
+    def export_midi(self, file_path, bpm=100, repeat=0, verbose=False):
         '''
         Outputs a pdf to a specified path
         '''
-        if file_name is None:
+        if file_path is None:
+            warnings.warn('MIDI not generated. Please specify valid path.',
+                          UserWarning)
+            traceback.print_stack()
+            return
+
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+
+        if file_path.endswith('/'):
             # Create filename based on key and time
-            file_name = (str(os.getcwd()) + '\output\\' +
+            file_path = (str(os.getcwd()) + '\output\\' +
                          str(datetime.now()).replace(' ', '_').replace(':', '.'))
 
         # Output a midi file
         if self.composition is not None and self.composition.tracks:
-            midi_file_out.write_Composition(file_name + '.mid', self.composition, bpm,
-                                            repeat, verbose)
+            midi_file_out.write_Composition(file_path + '.mid', self.composition,
+                                            bpm, repeat, verbose)
         else:
-            warnings.warn('PDF not generated because the composition didn\'t have any tracks. :(',
+            warnings.warn('MIDI not generated because the composition didn\'t' +
+                          ' have any tracks. :(', UserWarning)
+            traceback.print_stack()
+
+    def export_pickle(self, file_path, protocol_to_use=pickle.HIGHEST_PROTOCOL):
+        '''
+        Outputs a python pickled object to a specified path
+        '''
+
+        if file_path is None:
+            warnings.warn('Pickle not generated. Please specify valid path.',
                           UserWarning)
             traceback.print_stack()
+            return
+
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+
+        if file_path.endswith('/'):
+            # Create filename based on key and time
+            file_name = (str(os.getcwd()) + '\output\\' +
+                         str(datetime.now()).replace(' ', '_').replace(':', '.') +
+                         '.pkl')
+
+        with open(file_name, 'wb') as file:
+            pickle.dump(self, file, protocol=protocol_to_use)
+
+    @staticmethod
+    def from_pickle(pickle_file_name):
+        pkl_file = open(pickle_file_name, 'rb')
+        music_generator = pickle.load(pkl_file)
+        return music_generator
 
     def __str__(self):
         '''
         Returns a string representation of the class.
         '''
         output = ''
-        output += '----------------------------------------------------------------------------\n'
-        output += '------------------------------ Music Generator -----------------------------\n'
-        output += '----------------------------------------------------------------------------\n\n'
+        output += ('--------------------------------------------------------' +
+                   '--------------------\n')
+        output += ('------------------------------ Music Generator ---------' +
+                   '--------------------\n')
+        output += ('--------------------------------------------------------' +
+                   '--------------------\n\n')
         output += 'Time Signature: ' + str(self.__time_signature) + '\n'
         output += '           Key: ' + str(self.__key) + '\n'
         output += '\n'
@@ -260,7 +323,8 @@ class MusicGenerator(object):
                 # I guess I should start at 1... ugh.
                 index += 1
 
-                output += '================================== TRACK ' + str(index) + ' '
+                output += ('================================== TRACK ' +
+                           str(index) + ' ')
                 output += '=' * (35 - len(str(index)))
                 output += '\n'
 
@@ -268,43 +332,52 @@ class MusicGenerator(object):
                     # I guess I should start at 1... again... ugh.
                     index += 1
 
-                    output += '----------------------------------- Bar ' + str(index) + ' '
-                    # Adjust line based on size of index (ex: two less '-' for 2-digit number)
+                    output += ('----------------------------------- Bar ' +
+                               str(index) + ' ')
+                    # Adjust line based on size of index (ex: two less '-' for
+                    # 2-digit number)
                     output += '-' * (36 - len(str(index)))
                     output += '\n'
                     output += str(bar) + '\n'
 
-        output += '-----------------------------------------------------------------------------\n'
+        output += ('--------------------------------------------------------' +
+                   '---------------------\n')
         return output
 
     def _create_melody_timing(self, note_timing_dict):
         '''
-        Returns a list of note lengths representing the time of a melody for a single bar.
+        Returns a list of note lengths representing the time of a melody for a
+        single bar.
         '''
         melody_bar = []
 
-        # If valid time signature is supplied, use it to craft melody, otherwise use common time
+        # If valid time signature is supplied, use it to craft melody, otherwise
+        # use common time
         if not meter.is_valid(self.__time_signature):
             raise AttributeError('Time signature: ' + self.__time_signature +
                                  ' cannot be converted to a mingus meter. ' +
                                  'Use tuple (#, #) format. Ex: (4, 4)')
         else:
-            remaining_time_in_bar = time.get_time_remaining(melody_bar, self.__time_signature)
+            remaining_time_in_bar = time.get_time_remaining(melody_bar,
+                                                            self.__time_signature)
 
-            # Continue getting note progressions as long as they're room in the bar
+            # Continue getting note progressions as long as there's room in bar
             while (remaining_time_in_bar > 0.0):
-                next_timing = choice.choose_next_timing(remaining_time_in_bar, note_timing_dict)
+                next_timing = choice.choose_next_timing(remaining_time_in_bar,
+                                                        note_timing_dict)
 
                 melody_bar.append(next_timing)
 
-                remaining_time_in_bar = time.get_time_remaining(melody_bar, self.__time_signature)
+                remaining_time_in_bar = time.get_time_remaining(melody_bar,
+                                                                self.__time_signature)
 
         return melody_bar
 
     @staticmethod
     def _repeat_chords_track(raw_chord_progression, times_to_repeat):
         '''
-        Returns an extended raw chord list which is the original repeated a number of times
+        Returns an extended raw chord list which is the original repeated a
+        number of times
         '''
         # Repeat chords per argument
         extended_chord_progression = []
@@ -312,5 +385,3 @@ class MusicGenerator(object):
             for item in raw_chord_progression:
                 extended_chord_progression.append(item)
         return extended_chord_progression
-
-    
