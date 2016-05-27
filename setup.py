741bc142d95198a5f2edaa01c6b51b45ea3a5892
Alex <avantol13@gmail.com>
2016-05-26 20:00:45 -0400

    Update meta stuff, prep for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='music_generator',
+      version='0.1',
+      description='Generate randomized musical compositions based on probabilities.',
+      url='https://github.com/Avantol13/music_generator',
+      author='Alexander VanTol',
+      author_email='avantol13 at gmail dot com',
+      license='GNU GENERAL PUBLIC LICENSE V3',
+      packages=find_packages(),
+      keywords = "music generator theory probability random song melody tune",
+      include_package_data = True,
+      )
