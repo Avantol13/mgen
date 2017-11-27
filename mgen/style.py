331294bdfaf37600545b5d5a794ff15ee87ce83a
Alex <avantol13@gmail.com>
2017-11-26 23:17:46 -0600

    add versioning to json cfgs for styles

--- mgen/style.py
+++ mgen/style.py
@@ -12,6 +12,8 @@ import pprint
 DEFAULT_CFG_FILE = config._PATH_TO_SCRIPT + '/../styles/default.json'
 JAZZ_CFG_FILE    = config._PATH_TO_SCRIPT + '/../styles/jazz.json'
 
+REQUIRED_STYLE_CFG_VERSION = "1.0"
+
 class StyleProbs(object):
     '''
     A representation of a certain musical style. Holds the probabilities for
@@ -61,6 +63,17 @@ class StyleProbs(object):
 
         if 'minor_scales' not in self.probabilities.keys():
             raise Exception('Missing \'minor_scales\' section in cfg file.')
+        if (    ("__info__" not in data.keys())
+             or ("__version__" not in data["__info__"].keys()) ):
+            raise Exception("Missing \"__info__\" section in cfg file:"
+                            "\n   " + file_name)
 
         if 'major_scales' not in self.probabilities.keys():
             raise Exception('Missing \'major_scales\' section in cfg file.')
+        if data["__info__"]["__version__"] != REQUIRED_STYLE_CFG_VERSION:
+            raise Exception("Invalid style version: " +
+                            "{} in cfg file:\n   {}\n".format(
+                                data["__info__"]["__version__"], file_name
+                             ) + "This version of mgen requires a style " +
+                            "configuration with version: " +
+                            "{}\n".format(REQUIRED_STYLE_CFG_VERSION))
